c5a5c73993cb80cac31b96460494840906bfeb5b
Lukas Forst <lukas@forst.pw>
2020-04-09 22:11:19 +0200

    delete requiremets

--- common/Db.py
+++ common/Db.py
@@ -1,7 +1,9 @@
 import logging
 
 import redis
+
 from redis import Redis
+from sqlalchemy import create_engine
 
 from common.Config import Config, get_config
 from common.Utils import get_or_set
@@ -9,16 +11,16 @@ from common.Utils import get_or_set
 logger = logging.getLogger(__name__)
 
 
-def get_db() -> Redis:
+def get_redis() -> Redis:
     """
     Opens a new database connection if there is none yet for the
     current application context.
     """
     config = get_config()
-    return get_or_set('db', lambda: connect_db(config))
+    return get_or_set('db', lambda: connect_redis(config))
 
 
-def connect_db(config: Config) -> Redis:
+def connect_redis(config: Config) -> Redis:
     logger.debug(f'Connecting to the DB.')
     return redis.Redis(
         host=config.redis_url,
@@ -27,3 +29,12 @@ def connect_db(config: Config) -> Redis:
         password=config.redis_password,
         charset="utf-8",
         decode_responses=True)
+
+
+def get_postgres():
+    config = get_config()
+    engine = create_engine('postgresql+psycopg2://user:password@hostname/database_name')
+
+
+def connect_postgres(config: Config):
+    engine = create_engine('postgresql+psycopg2://user:password@hostname/database_name')
