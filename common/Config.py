d1c387cf2430e178c15ed0ef51a5b33ddf0445ab
Lukas Forst <lukas@forst.pw>
2020-04-09 21:52:32 +0200

    load correctly redis condif

---
+++ common/Config.py
@@ -0,0 +1,52 @@
+import logging
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+from flask import current_app as app
+
+from common.Utils import get_or_set
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Config:
+    """
+    Redis configuration
+    """
+    redis_url: str
+    redis_port: int
+    redis_username: Optional[str]
+    redis_password: Optional[str]
+
+
+def get_config() -> Config:
+    """
+    Obtains configuration from the application context.
+    """
+    return get_or_set('config', build_configuration)
+
+
+def build_configuration() -> Config:
+    """
+    Builds configuration from environment or from the Flask properties
+    """
+    logger.debug('Building configuration.')
+    config = Config(redis_url=get_prop('REDIS_URL'),
+                    redis_port=int(get_prop('REDIS_PORT')),
+                    redis_username=get_prop('REDIS_USERNAME', True),
+                    redis_password=get_prop('REDIS_PASSWORD', True))
+    logger.debug(f'Used configuration: {config}')
+    return config
+
+
+def get_prop(name: str, optional: bool = False) -> str:
+    """
+    Gets property from environment or from the flask env.
+    """
+    config = os.environ.get(name, app.config.get(name))
+    if not optional and not config:
+        logger.error(f'It was not possible to retrieve configuration for property "{name}"!')
+        raise EnvironmentError(f'No existing configuration for "{name}" found!')
+    return config
---
+++ common/Utils.py
@@ -0,0 +1,11 @@
+from typing import Callable
+
+
+def get_or_set(prop: str, factory: Callable):
+    """
+    Gets or sets context property.
+    """
+    if not hasattr(g, prop):
+        setattr(g, prop, factory())
+
+    return getattr(g, prop)
