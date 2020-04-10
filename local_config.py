e65babe14a30d76f82f9441d273d64528540d3c3
Lukas Forst <lukas@forst.pw>
2020-04-10 08:48:26 +0200

    add local config

---
+++ local_config.py
@@ -0,0 +1,9 @@
+REDIS_URL = 'localhost'
+REDIS_PORT = '6379'
+REDIS_USERNAME = ''
+REDIS_PASSWORD = ''
+
+POSTGRES_USER = 'super-cool-datavid19'
+POSTGRES_PASSWORD = 'super-secret-pwd'
+POSTGRES_DB = 'datavid'
+POSTGRES_URL = 'localhost:5432'
